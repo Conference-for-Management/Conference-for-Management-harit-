429b4cbf17e24f386b510cf6577812adf577262e
harit <harit@gmail.com>
2026-03-03 12:24:53 +0000

    initialize project structure with Spring Boot and Maven setup

---
+++ src/main/java/com/example/gestion_de_conferences/GestionDeConferencesApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestion_de_conferences;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeConferencesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionDeConferencesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gestion_de_conferences/GestionDeConferencesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestion_de_conferences;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeConferencesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
