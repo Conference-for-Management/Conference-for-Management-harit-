306b34831caffc93e45b2176b821ac73960fdf46
harit <harit@gmail.com>
2026-03-03 12:54:10 +0000

    configure application properties and enable Feign clients

--- src/main/java/com/example/gestion_de_conferences/GestionDeConferencesApplication.java
+++ src/main/java/com/example/gestion_de_conferences/GestionDeConferencesApplication.java
@@ -2,8 +2,11 @@ package com.example.gestion_de_conferences;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
+@EnableFeignClients
 public class GestionDeConferencesApplication {
 
     public static void main(String[] args) {
